4e5652caddc6d81a3f16c2de476384fb5e1a8b7d
root <1098582358@qq.com>
2023-07-24 18:03:38 +0800

    测试类

--- advice/src/test/java/com/woldier/TestMp.java
+++ advice/src/test/java/com/woldier/TestMp.java
@@ -6,6 +6,8 @@ import com.woldier.entity.Plaza;
 import com.woldier.entity.vo.PlazaVo;
 import com.woldier.mapper.PlazaMapper;
 import com.woldier.mapper.UserMapper;
+import com.woldier.service.PlazaService;
+import com.woldier.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,13 +17,48 @@ import java.util.List;
 @SpringBootTest
 public class TestMp {
     @Autowired
-    private PlazaMapper plazaMapper;
+    UserMapper userMapper;
     @Autowired
-    private UserMapper userMapper;
+    PlazaMapper plazaMapper;
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    PlazaService plazaService;
+
     @Test
-    public void test(){
+    public void test() {
 //        userMapper.selectById(1);
-        Page<Plaza> plazaVoPage = new Page<>(1, 5);
+        Page<Plaza> plazaPage = new Page<>(1, 5);
+        Page<Plaza> page = plazaService.page(plazaPage);
+        System.out.println(page);
+
     }
+
+    /**
+    *
+    * description 基于mp重写page的查询实现多表查询,这样做需要自定义一个vo,以及查询的sql,不过sql的格式基本是一致的
+    *
+    * @author: woldier
+    * @date: 2023/7/24 下午4:47
+    */
+    @Test
+    public void test2() {
+        Page<PlazaVo> plazaPage = new Page<>(1, 5);
+        QueryWrapper<PlazaVo> qw = new QueryWrapper<>();
+        //TODO qw可以加入客制化查询条件
+        qw.ge("u.id",10);
+        qw.ge("p.id",50);
+        List<PlazaVo> page = plazaMapper.getPlazaPage(plazaPage, qw);
+        System.out.println(page);
+    }
+
+
+    @Test
+    public void test3(){
+        Page<PlazaVo> page = plazaService.getPage(1, 5);
+        System.out.println(page);
+    }
+
 
 }
