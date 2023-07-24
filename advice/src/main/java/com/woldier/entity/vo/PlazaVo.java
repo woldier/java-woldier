8321c16165e86cc1efd77bcb7e86027075d5faa5
root <1098582358@qq.com>
2023-07-24 18:02:38 +0800

    plaza vo类

--- advice/src/main/java/com/woldier/entity/vo/PlazaVo.java
+++ advice/src/main/java/com/woldier/entity/vo/PlazaVo.java
@@ -1,7 +1,9 @@
 package com.woldier.entity.vo;
 
 import com.woldier.entity.Plaza;
+import lombok.Data;
 
+@Data
 public class PlazaVo extends Plaza {
     private String nikeName; //拓展的user字段
     private String img; //拓展的user字段
