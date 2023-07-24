217ed25881619a11a115c7027f3523c11a16c9fb
root <1098582358@qq.com>
2023-07-24 18:03:41 +0800

    测试类

--- advice/src/main/java/com/woldier/service/impl/UserServiceImpl.java
+++ advice/src/main/java/com/woldier/service/impl/UserServiceImpl.java
@@ -3,6 +3,9 @@ package com.woldier.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.woldier.entity.User;
 import com.woldier.mapper.UserMapper;
+import com.woldier.service.UserService;
+import org.springframework.stereotype.Service;
 
-public class UserServiceImpl extends ServiceImpl<UserMapper, User> {
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
