835a250d5d3493d2860923eac3bec53787cfdcb5
root <1098582358@qq.com>
2023-07-29 08:30:14 +0800

    redis配置类

---
+++ redis-lock/src/main/java/com/wolder/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.wolder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+/**
+*
+* description redis消息监听并配置类
+*        
+* @author: woldier 
+* @date: 2023/7/28 上午9:03
+*/
+@Configuration
+public class RedisConfig {
+//    @Bean
+//    public MessageListenerAdapter listenerAdapter(MessageReceiver messageReceiver){
+//        MessageListenerAdapter adapter = new MessageListenerAdapter(messageReceiver,"onMessage");
+//        return adapter;
+//    }
+    @Bean
+    public RedisMessageListenerContainer listenerContainer(RedisConnectionFactory factory,MessageReceiver messageReceiver){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        MessageListenerAdapter adapter = new MessageListenerAdapter(messageReceiver,"onMessage");
+        container.addMessageListener(adapter, new PatternTopic("topic"));
+        return container;
+
+    }
+
+}
