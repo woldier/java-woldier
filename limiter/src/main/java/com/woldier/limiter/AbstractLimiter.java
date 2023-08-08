1e69fe80bc83121b71ee6e6d0065736a14506283
woldier <1098582358@qq.com>
2023-08-08 13:25:57 +0800

    抽象类

--- limiter/src/main/java/com/woldier/limiter/AbstractLimiter.java
+++ limiter/src/main/java/com/woldier/limiter/AbstractLimiter.java
@@ -1,14 +1,19 @@
 package com.woldier.limiter;
 
+import sun.misc.Unsafe;
+
+import java.io.Serializable;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
 /**
  * description 抽象类,定义了一些控制的常量
  *
  * @author: woldier wong
  * @date: 2023/8/7$ 20:59$
  */
-public abstract class AbstractLimiter implements Limiter {
-    protected long timeStamp = System.currentTimeMillis();
-    protected  int  requestCount; //请求次数
+public abstract class AbstractLimiter implements Limiter, Serializable {
+    protected volatile long timeStamp = System.currentTimeMillis();
+    protected volatile int  requestCount; //请求次数
     protected final  int limitNum ;//最大限流数
 
     protected final long interval ;//时间窗口市场,单位ms
@@ -18,5 +23,10 @@ public abstract class AbstractLimiter implements Limiter {
         this.limitNum = limitNum;
         this.interval = interval;
     }
+
+
+
+
+
 
 }
